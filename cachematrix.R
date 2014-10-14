ad59db3cc937bbc55fe8ea5f9325b3cfa4849ea8
vivekrathi7 <vivekrathi.iitr@gmail.com>
2014-10-14 08:58:36 +0530

    Updated the functions for caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## makeCacheMatrix: Caches the matrix and cachesolve: Computes / Gets the inverse from cache
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function (x = matrix()) {
+  inv <- matrix()
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv2) inv <<- inv2
+  getInv <- function() inv
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: This function calculates the inverse of a matrix
+## If inverse of the matrix has already been calculated (and the matrix has not changed), get the inverse from cache and skip computation
+## Otherwise, calculate the inverse of the matrix and set the value of inverse in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  m <- x$get()
+  epsilon <- 1e-6
+  #inv has the result of invocation to getInvx() function of makeCacheMatrix
+  #This makes: class(x) a "list"
+  if(!is.na(inv[[1]])) {
+    message("getting cached data")
+    return(inv)
+  }
+  #Otherwise, get data, calculate inverse, and set the inverse
+  else if (abs(det(m))<epsilon) {
+    message("Non invertible Matrix: System is exactly singular")
+  } else {
+    inv <- solve(m)
+    x$setInv(inv)
+    return(inv)
+  } 
 }
+
+##Testing: l <- makeCacheMatrix(matrix(c(2,3,2,3), nrow=2)); cacheSolve(l) #Singular Matrix
+##Testing: li <- makeCacheMatrix(matrix(c(2,3,2,2), nrow=2)); cacheSolve(li) #Normal output
